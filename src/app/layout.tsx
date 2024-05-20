23e04fcff9e614e428a56e059efd5582b921d5c7
alaminpu1007 <alamin66.sit@gmail.com>
2024-05-20 10:33:22 +0600

    umami analytics is integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,10 +37,9 @@ export default function RootLayout({
         <FooterComponent />
       </body>
       <Script
-        async
         defer
-        src='https://cloud.umami.is/script.js'
-        data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID || ''}
+        src='https://umami-rho-henna.vercel.app/script.js'
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
       />
     </html>
   );
