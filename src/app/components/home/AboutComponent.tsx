e59a277edacca9d7fea9018ea26955262caf0e35
alaminpu1007 <alamin66.sit@gmail.com>
2026-05-24 21:22:36 +0600

    updated career summary

--- src/app/components/home/AboutComponent.tsx
+++ src/app/components/home/AboutComponent.tsx
@@ -19,7 +19,7 @@ const AboutComponent = () => {
         <h1 className='title-txt'>About Me</h1>
         <p className='text-theme-primary dark:text-theme-dark-primary m-0 mt-3 p-0 text-lg break-words'>
           {
-            "I'm a 27-year-old software engineer with 5+ years of experience in web technology, specializing in JavaScript, React.js (Next.js), and problem-solving. I am passionate about creating high-performance web applications and continuously improving my skills in modern web development technologies."
+            'Software Engineer with 5+ years of experience in JavaScript, React.js, Next.js, and modern web technologies. Skilled in building scalable, responsive, and high-performance web applications with a strong focus on clean code, UI/UX, and problem-solving. Experienced working on SaaS, CRM, e-commerce, and real-time applications using React, TypeScript, Redux, Node.js, and REST APIs. Passionate about learning new technologies and solving complex problems through data structures and algorithms.'
           }
         </p>
         {/* <p className='m-0 mt-3 break-words p-0 text-lg text-theme-primary dark:text-theme-dark-primary'>
